534715340042df6b7698dd46439910fbbfcd2b2b
Schen <schen@jerseymikes.com>
2019-08-01 13:29:42 -0400

    update recommend tag list

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -50,6 +50,8 @@ export const RECOMMEND_TAGS = List([
     'aaa:影评',
     'sportstalk:体育',
     'realityhubs:产品评价',
+	'科学技术:cn-stem steemstem',
+	'编程:cn-stem steemstem cn-programming'
 ]);
 
 export const LIQUID_TOKEN = 'Steem';
