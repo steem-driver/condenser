96f702a09e02516630b4a8cbf63ba382a01c9fd1
Song <schen@jerseymikes.com>
2023-08-04 15:49:05 -0400

    new domain

--- src/app/components/modules/Settings.jsx
+++ src/app/components/modules/Settings.jsx
@@ -215,7 +215,7 @@ class Settings extends React.Component {
 
     generateAPIEndpointOptions = () => {
         let endpoints = [
-            'https://api.steem.buzz',
+            'https://api.steemcn.xyz',
             'https://cn.steems.top',
             'https://api.justyy.com',
             'https://api.steemitdev.com',
@@ -226,7 +226,7 @@ class Settings extends React.Component {
         if (typeof window !== 'undefined')
             preferred_api_endpoint =
                 localStorage.getItem('user_preferred_api_endpoint') === null
-                    ? 'https://api.steem.buzz'
+                    ? 'https://api.steemcn.xyz'
                     : localStorage.getItem('user_preferred_api_endpoint');
         if (endpoints === null || endpoints === undefined) {
             return null;
@@ -270,11 +270,11 @@ class Settings extends React.Component {
             progress,
         } = this.state;
 
-        let preferred_api_endpoint = 'https://api.steem.buzz';
+        let preferred_api_endpoint = 'https://api.steemcn.xyz';
         if (typeof window !== 'undefined') {
             preferred_api_endpoint =
                 localStorage.getItem('user_preferred_api_endpoint') === null
-                    ? 'https://api.steem.buzz'
+                    ? 'https://api.steemcn.xyz'
                     : localStorage.getItem('user_preferred_api_endpoint');
         }
 
